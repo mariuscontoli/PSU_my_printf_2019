4e919ef288915ecbb54743ead15e4c702b9df975
mariuscontoli <marius.contoli-legay@epitech.eu>
2019-11-08 12:08:46 +0100

    add_header_file

---
+++ include/my_printf.h
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2019
+** header
+** File description:
+** my_printf
+*/
+
+#ifndef MY_PRINTF_H
+#define MY_PRINTF_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int my_putstr(char const *str);
+int my_put_nbr(int nb);
+int my_getnbr(char const *str);
+void my_putchar(char c);
+void print_str(va_list ap);
+void print_char(va_list ap);
+void print_nbr(va_list ap);
+void my_printf(char *s, ...);
+
+#endif
