a45037f9e7ec18f320a9905ed0c45706f165c6b7
Marius <marius.contoli-legay@epitech.eu>
2019-11-09 14:07:05 +0100

    added_function_unsigned_int_put_nbr

--- utils.c
+++ utils.c
@@ -50,6 +50,25 @@ int my_put_nbr(int nb)
     }
 }
 
+unsigned int my_put_nbr_uns(unsigned int nb)
+{
+    int modulo;
+    if (nb < 0) {
+        my_putchar('-');
+        nb = nb * (-1);
+    }
+    if (nb >= 0) {
+        if (nb >= 10) {
+            modulo = (nb % 10);
+            nb = (nb - modulo) / 10;
+            my_put_nbr_uns(nb);
+            my_putchar(48 + modulo);
+        }
+        else
+            my_putchar(48 + nb % 10);
+    }
+}
+
 int my_getnbr(char const *str)
 {
     int i = 0;
