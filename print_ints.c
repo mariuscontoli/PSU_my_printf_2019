9e044771637a0fa4df7a9ce4050a065b2880961b
Marius <marius.contoli-legay@epitech.eu>
2019-11-10 17:09:11 +0100

    add_hexa_func

--- print_ints.c
+++ print_ints.c
@@ -24,3 +24,17 @@ void print_nbr(va_list ap)
     nb = va_arg(ap, int);
     my_put_nbr(nb);
 }
+
+void print_nbr_hex(va_list ap)
+{
+    int nb;
+    nb = va_arg(ap, int);
+    my_put_nbr_hex(nb);
+}
+
+void print_nbr_hexx(va_list ap)
+{
+    int nb;
+    nb = va_arg(ap, int);
+    my_put_nbr_hexx(nb);
+}
