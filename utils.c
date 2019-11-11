fbeee9850557fe097eac53d4a1408442beb6b5b4
Marius <marius.contoli-legay@epitech.eu>
2019-11-11 14:15:38 +0100

    split_file_in_second_part_to_respect_coding_style

--- utils.c
+++ utils.c
@@ -7,6 +7,7 @@
 
 #include <unistd.h>
 #include <stdlib.h>
+#include "include/my_printf.h"
 
 void my_putchar(char c)
 {
@@ -29,80 +30,6 @@ int my_strlen(char const *str)
         ++i;
     }
     return i;
-}
-
-int my_put_nbr(int nb)
-{
-    int modulo;
-    if (nb < 0) {
-        my_putchar('-');
-        nb = nb * (-1);
-    }
-    if (nb >= 0) {
-        if (nb >= 10) {
-            modulo = (nb % 10);
-            nb = (nb - modulo) / 10;
-            my_put_nbr(nb);
-            my_putchar(48 + modulo);
-        }
-        else
-            my_putchar(48 + nb % 10);
-    }
-}
-
-int my_put_nbr_hex(int nb)
-{
-    int modulo;
-    if (nb < 0) {
-        my_putchar('-');
-        nb = nb * (-1);
-    }
-    if (nb > 0) {
-        modulo = (nb % 16);
-        nb = (nb - modulo) / 16;
-        my_put_nbr_hex(nb);
-        if (modulo <= 9)
-            my_putchar(48 + modulo);
-        else
-            my_putchar(87 + modulo);
-    }
-}
-
-int my_put_nbr_hexx(int nb)
-{
-    int modulo;
-    if (nb < 0) {
-        my_putchar('-');
-        nb = nb * (-1);
-    }
-    if (nb > 0) {
-        modulo = (nb % 16);
-        nb = (nb - modulo) / 16;
-        my_put_nbr_hex(nb);
-        if (modulo <= 9)
-            my_putchar(48 + modulo);
-        else
-            my_putchar(55 + modulo);
-    }
-}
-
-unsigned int my_put_nbr_uns(unsigned int nb)
-{
-    int modulo;
-    if (nb < 0) {
-        my_putchar('-');
-        nb = nb * (-1);
-    }
-    if (nb >= 0) {
-        if (nb >= 10) {
-            modulo = (nb % 10);
-            nb = (nb - modulo) / 10;
-            my_put_nbr_uns(nb);
-            my_putchar(48 + modulo);
-        }
-        else
-            my_putchar(48 + nb % 10);
-    }
 }
 
 int my_getnbr(char const *str)
