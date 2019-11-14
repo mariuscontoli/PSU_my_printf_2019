7afb84a40bcaeb401ae29ee70e5ed08d8ddb1696
Marius <marius.contoli-legay@epitech.eu>
2019-11-14 18:58:17 +0100

    function for big S flag added

--- utils.c
+++ utils.c
@@ -23,6 +23,22 @@ int my_putstr(char const *str)
     }
 }
 
+int my_putsstr(char const *str)
+{
+    int i = 0;
+    int test = 0;
+    while (str[i] != '\0') {
+        if (str[i] < 32 || str[i] > 126) {
+            my_putchar('\\');
+            my_put_nbr_oct(str[i]);
+            i += 1;
+        } else {
+            my_putchar(str[i]);
+            i += 1;
+        }
+    }
+}
+
 int my_strlen(char const *str)
 {
     int i = 0;
