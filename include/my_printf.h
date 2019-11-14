41df91777db7df7fde94c33483be06d7ef3787fd
Marius <marius.contoli-legay@epitech.eu>
2019-11-14 19:28:33 +0100

    add protos for binary base function

--- include/my_printf.h
+++ include/my_printf.h
@@ -16,7 +16,6 @@
 int my_putstr(char const *str);
 int my_putsstr(char const *str);
 int my_put_nbr(int nb);
-int my_getnbr(char const *str);
 void my_putchar(char c);
 void print_str(va_list ap);
 void print_sstr(va_list ap);
@@ -26,7 +25,9 @@ void print_nbr_hex(va_list ap);
 void print_nbr_hexx(va_list ap);
 void print_nbr_oct(va_list ap);
 void print_uns_nbr(va_list ap);
+void print_nbr_bin(va_list ap);
 unsigned int my_put_nbr_uns(unsigned int nb);
+unsigned int my_put_nbr_bin(unsigned int nb);
 int my_put_nbr_oct(int nb);
 int my_put_nbr_hex(int nb);
 int my_put_nbr_hexx(int nb);
